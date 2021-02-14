1c415b0d30a9cc9302c1efc404ca2c3dcc4a55a3
emma <emmalcgreen@gmail.com>
2021-02-14 13:24:15 -0500

    cleaned up some scss with mixins and redid hamburger menu in html, added navigation styles and JS functionality

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const mobileNav = document.querySelector('.nav-links');
+    const navLi = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', function (){
+        //animate burger icon
+        burger.classList.toggle('open');
+        //have nav appear
+        mobileNav.classList.toggle('nav-open');
+
+        //animate links
+    navLi.forEach((link, index)=>{
+        if(link.style.animation){
+            link.style.animation = ''
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+    });
+
+    });
+}
+
+navSlide();
+
